0b8ecbfb21cd5cdde6269a9de687564fae46e9b3
Elijah Budd <eabudd@students.unwsp.edu>
2026-03-05 17:49:05 -0600

    Update Code.java

--- Code.java
+++ Code.java
@@ -6,19 +6,21 @@ public class Pascal_App {
 		
 		System.out.println("Pascal's Triangle:");
 		
-		int[][] pascalGrid = new int[11][11];
+		int[][] pascalGrid = new int[11][11]; // Create multi dimensional array
 		
+		// Build Pascal's Triangle
 		for (int row = 0; row <= 10; row++) {
 			for (int col = 0; col <= row; col++) {
 				
 				if (col == 0 || col == row) {
-					pascalGrid[row][col] = 1;
+					pascalGrid[row][col] = 1; // Edges are always a 1
 				} else {
 					pascalGrid[row][col] = pascalGrid[row - 1][col] + pascalGrid[row - 1][col - 1];
 				}
 			}
 		}
 		
+		// Print Pascal's Triangle
 		for (int row = 0; row <= 10; row++) {
 			for (int col = 0; col<= row; col++) {
 				System.out.print(pascalGrid[row][col] + " ");
